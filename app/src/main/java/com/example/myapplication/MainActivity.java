a9f64a016875730edc06a12aaf0d2805c8c486fd
Cecep Mulyana Dipraja <diprajaarmy94@gmail.com>
2019-10-15 11:44:20 +0700

    tambah jumlah

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int jumlahA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tambah3jumlahP(View view) {
+        jumlahA = jumlahA +3;
+        TextView TextViewP = findViewById(R.id.jumlahA);
+        TextViewP.setText(String.valueOf(jumlahA));
     }
 }
